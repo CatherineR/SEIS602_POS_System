0749f5ab37fdaccb77e90d9cf96cf9a895865e81
olaniaga <olaniaga@10.0.0.36>
2016-10-21 14:54:18 -0500

    Added Cashier and manager class which inherit properties from employee. Edited the Employee class and its test cases.

--- POS_System/src/Employee.java
+++ POS_System/src/Employee.java
@@ -1,4 +1,12 @@
 
 public class Employee {
+	private String userName ;
+	private String password;
+	private String role;
 
+	//default constructor
+	public Employee(){
+		//test creation of Employee
+		System.out.println("created a new object " + this.toString());
+	}
 }
--- POS_System/tests/EmployeeTests.java
+++ POS_System/tests/EmployeeTests.java
@@ -1,4 +1,6 @@
 
 public class EmployeeTests {
-
+	public static void main(String args[]){
+		Employee emp1 = new Employee();
+	}
 }
