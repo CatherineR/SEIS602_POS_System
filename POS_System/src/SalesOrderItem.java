23548c4dbe8a93c72fec88ef5d363f955bb6d213
wenjingggg <lu003374@stthomas.edu>
2016-10-23 15:40:12 -0500

    Add files via upload

--- POS_System/src/SalesOrderItem.java
+++ POS_System/src/SalesOrderItem.java
@@ -1,8 +1,33 @@
-
 public class SalesOrderItem {
 	private String name;
 	private int orderQuantity;
 	private double itemPrice;
 	private double subTotal;
 	private double taxRate;
+	private String ItemID;
+	
+
+
+public void setItemID(String name,double itemPrice,double taxRate){
+	   this.name=name;
+	   this.itemPrice=itemPrice;
+	   this.taxRate=taxRate;
+}
+public String getItemID(){
+	   return this.ItemID;
+}
+public String getname(){
+	   return this.name;
+}
+public double gettaxRate(){
+	   return this.taxRate;
+}
+public void setprice(double price){
+	   this.itemPrice=itemPrice;
+}
+public double getprice(){
+	   return this.itemPrice;
+}
+
+
 }
