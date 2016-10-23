3e989161ae490740b2de5b19c360db0637e433b1
Olani <olaniaga@gmail.com>
2016-10-23 18:20:16 -0500

    Edit supplier class.
    Added and defined constructors and getters/setters.

--- POS_System/src/Supplier.java
+++ POS_System/src/Supplier.java
@@ -7,6 +7,39 @@ public class Supplier {
 	private Date lastOrderDate;
 	private String address;
 	private String phoneNumber;
+	
+	public Supplier(String supplierName, Date firstOrderDate, Date lastOrderDate,
+					String address, String phoneNumber){
+		this.supplierName = supplierName;
+		this.firstOrderDate = firstOrderDate;
+		this.lastOrderDate = lastOrderDate;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public void addSupplierToFile(Supplier supplier){
+		
+	}
 
+	public String getSupplierName() {
+		return supplierName;
+	}
+
+	public Date getFirstOrderDate() {
+		return firstOrderDate;
+	}
+
+	public Date getLastOrderDate() {
+		return lastOrderDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
 	
 }
