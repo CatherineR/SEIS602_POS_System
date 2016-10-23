4acea54ad6832293171cc550e116da4709fcabc7
olaniaga <olaniaga@10.48.68.58>
2016-10-23 12:27:22 -0500

    Remove login functionality from employee class and edit (compensate for employee class changes) in Register class. Test cases for register and employee have also been updated.

--- POS_System/tests/EmployeTestCases.java
+++ POS_System/tests/EmployeTestCases.java
@@ -4,48 +4,5 @@ import org.junit.Test;
 
 public class EmployeTestCases {
 
-	@Test
-	public void test1() {
-		String userName = "olaniCashier";
-		String password = "1234";
-		Employee emp1 = new Employee(userName, password);
-		
-		assertEquals("cashier",emp1.login(userName, password).toLowerCase(), "cashier");
-		
-
-	}
-	
-	@Test
-	public void test2() {
-		String userName = "olaniManager";
-		String password = "1234";
-		Employee emp2 = new Employee(userName, password);
-		//System.out.println(emp1.login(userName, password));
-		
-		assertEquals("manager",emp2.login(userName, password).toLowerCase(), "manager");
-		
-	}
-	
-	@Test
-	public void test3() {
-		String userName = "olaniManager1";
-		String password = "1234";
-		Employee emp2 = new Employee(userName, password);
-		//System.out.println(emp1.login(userName, password));
-		
-		assertEquals("not available",emp2.login(userName, password).toLowerCase(), "not available");
-		
-	}
-	
-	@Test
-	public void test4() {
-		String userName = "olaniManager";
-		String password = "1235";
-		Employee emp2 = new Employee(userName, password);
-		//System.out.println(emp1.login(userName, password));
-		
-		assertEquals("not available",emp2.login(userName, password).toLowerCase(), "not available");
-		
-	}
 
 }
