dc19104a0fe995a76419439cd091506b60f2ca87
Olani <olaniaga@gmail.com>
2016-10-30 13:42:28 -0500

    Removed old SalesOrder Constructor.
    Removed older SalesOrder constructor and added new (default)
    constructor.

--- POS_System/src/SalesOrder.java
+++ POS_System/src/SalesOrder.java
@@ -11,59 +11,60 @@ public class SalesOrder {
 	private int numItem;
 	
 	
-	 
-	public SalesOrder(SalesOrderItem item){
-			this.item=item;
-			setNumItem(1);
-		}
-	
-		public SalesOrderItem getItem(){
-			return this.item;
-		}
-		
-		public void setSaleorderID(UUID salesOrderID){
-			this.salesOrderID=salesOrderID;
-		}
-		
-		public UUID getSaleorderID(){
-			return salesOrderID;
-		}
-		public void setNumItem(int numItem){
-			this.numItem=numItem;
-		}
-		public int getNumItem(){
-			return this.numItem;
-		}
-		public int getQuantity(){
-			return (getItem().getorderQuantity());
-		}
-		
-		public String getname(){
-			return (getItem().getname());
-		}
-		
-		public double getprice(){
-			return (getItem().getprice());
-		}
-		public double gettaxRate(){
-			return(getItem().gettaxRate());
-		}
-		public double setTotalCprice(){
-			return totalPrice=((item.getprice()*(1+item.gettaxRate())*getNumItem()));
-		}
-		public double getTotalprice(){
-			return this.totalPrice;
-		}
-	
-		public void setItemList(List<SalesOrderItem> itemList) {
-					
-			this.itemList = itemList;
-		}
-		public List<SalesOrderItem> getItemList() {
-			return itemList;
-		}
-		
-		public void cancelOrder(){
-			setNumItem(0);
-		}
+	public SalesOrder(){
+		salesOrderID = UUID.randomUUID();
+		numItem =0;
+		totalPrice = 0.0;
+		itemList = new ArrayList<SalesOrderItem>();
 	}
+	
+	public SalesOrderItem getItem(){
+		return this.item;
+	}
+	
+	public void setSaleorderID(UUID salesOrderID){
+		this.salesOrderID=salesOrderID;
+	}
+	
+	public UUID getSaleorderID(){
+		return salesOrderID;
+	}
+	public void setNumItem(int numItem){
+		this.numItem=numItem;
+	}
+	public int getNumItem(){
+		return this.numItem;
+	}
+	public int getQuantity(){
+		return (getItem().getorderQuantity());
+	}
+	
+	public String getname(){
+		return (getItem().getname());
+	}
+	
+	public double getprice(){
+		return (getItem().getprice());
+	}
+	public double gettaxRate(){
+		return(getItem().gettaxRate());
+	}
+	public double setTotalCprice(){
+		return totalPrice=((item.getprice()*(1+item.gettaxRate())*getNumItem()));
+	}
+	public double getTotalprice(){
+		return this.totalPrice;
+	}
+
+	public void setItemList(List<SalesOrderItem> itemList) {
+				
+		this.itemList = itemList;
+	}
+	public List<SalesOrderItem> getItemList() {
+		return itemList;
+	}
+	
+	public void cancelOrder(){
+		setNumItem(0);
+	}
+}
