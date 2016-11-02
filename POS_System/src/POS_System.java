10e1a5055ff2cb0fa526d376399ce27f3cc83d98
wenjingggg <lu003374@stthomas.edu>
2016-11-01 23:23:47 -0500

    Add files via upload
    added salesorder menu

--- POS_System/src/POS_System.java
+++ POS_System/src/POS_System.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class POS_System {
 	
-
+	
 	Register register2 = new Register();
+	
 	public static void main(String[] args){
 		Register register1 = new Register();
 	    Scanner input = new Scanner(System.in);
-
+	    
 		System.out.println("Welcome to the Point-Of-Sale Registration System" + "\n");
 		String inputText;
 		do{
@@ -51,18 +52,84 @@ public class POS_System {
 						command = input.nextLine();
 						if(command.equals("1")){
 							startSalesMenu();
+							
+							}
+							
 						}					
 					}while(!command.equals("0"));//end Cashier menu
 				}
 				register1.logout();
 			}
-		}while(!inputText.equals("Quit"));
+		while(!inputText.equals("Quit"));
 		System.out.println("Goodbye");
 	 
 	}
 	private static void startSalesMenu(){
-		//TODO Sales Order Menu and commands go here
+		System.out.println("Type in '1' if you would like to begin scanning items");
+		//System.out.println("Type in '2' if you would like to check out");
+		System.out.println("Type in '2' if you would like to return items");	
+		System.out.println("Type in '3' if you would like to go back to main menu");	
+		Scanner input=new Scanner(System.in);
+		int choise=input.nextInt();
+		if(choise==1)
+		{
+			doScan();
+		}
+
+		if(choise==2)
+		{
+		//Register.removeItem();
+			
+			register1.removeItem(name, quantity);
+		}
+		if(choise==3){
+			//exit to main menu
+		}
 	}
+	private static void doScan(){
+		System.out.println("************Scanning begins************");
+				Register register1=new Register();
+				register1.beginTransaction();
+				Scanner input = new Scanner(System.in);
+				do
+				{
+					System.out.println("Input the item's name  then press enter");
+					String name=input.next();
+					System.out.println("Input the item's quantity then press enter");
+				    int quantity=input.nextInt();
+					System.out.println("Enter 'Y' to continue or enter 'N' to check out ");
+					if(input.next().equals("Y"))
+					{
+					register1.addItem(name, quantity);
+					
+							                           }
+					else if(input.next().equals("N"))
+							
+					{	
+						doCheckOut();
+						}
+				}
+				while(true);
+				
+	}
+	private static void doCheckOut(){
+			System.out.println("*************Check out ************");
+			//print salesorder(receipt) and total price
+			Register register1=new Register();
+			register1.commitTransaction();
+			System.out.println("Cashier No.");
+			System.out.println("Total price:");
+			//tracks payment 
+			register1.receivePayment(register1.totalSale());
+			
+	}
+		
+						
+				
+			
+			
+		
+	
 	private static void startInventoryManagerMenu(){
 		Manager manager = new Manager();
 		String choice;
