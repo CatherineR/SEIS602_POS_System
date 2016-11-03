65e9230f4a50dc23e466f9e98c798d857d857f56
jonyeneho <jeo2006@netscape.net>
2016-11-03 17:14:01 -0500

    Updated EmployeeTests

--- POS_System/tests/EmployeeTests.java
+++ POS_System/tests/EmployeeTests.java
@@ -1,8 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class EmployeeTests {
-	public static void main(String args[]){
-		Employee emp1 = new Employee();
-		//test creation of Employee
+
+	
+	@Test
+	public void testEmployee() {
+		
+		Employee emp1 = new Employee("olaniCashier", "1234");
 		System.out.println("created a new object " + emp1.toString());
+		Employee emp2 = new Employee("traineeCashier", "5678");
+		System.out.println("created a new object " + emp2.toString());
+		assertEquals("olaniCashier", emp1.getUserName());
+		assertEquals("1234", emp1.getPassword());
+		assertEquals("traineeCashier", emp2.getUserName());
+		assertEquals("5678", emp2.getPassword());
 	}
+
 }
