c5950b1d9762416d448242d450bdaaf2f6989791
wenjingggg <lu003374@stthomas.edu>
2016-11-02 22:28:12 -0500

    Add files via upload
    added changed method test

--- POS_System/tests/SalesOrderTest.java
+++ POS_System/tests/SalesOrderTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,47 +14,67 @@ public class SalesOrderTest {
 
 	@Test
 	public void test() {
-		SalesOrderItem s1=new SalesOrderItem(1.0,"name");
-		SalesOrderItem s2=new SalesOrderItem(2.0,"name2");
+		SalesOrderItem s1=new SalesOrderItem("bread",1);
+		SalesOrderItem s2=new SalesOrderItem("cheese",1);
 		s1.setItemID("1");
 		s2.setItemID("2");
 		s1.getItemID();
-		InventoryItem i1=new InventoryItem("name",1,1.0,"supplier",0.95,1);
-		System.out.println(s1.getname());
-		System.out.println(s1.getorderQuantity());
-		System.out.println(i1.getName());
-		System.out.println(i1.getPrice());
-		Inventory inventory=new Inventory();
-		InventoryListDAO invListDAO = new InventoryListDAO();		
-		invListDAO.addItem("name", 20, 2.50, "Kemps", 0, 10);	
-		inventory.getInventoryItem("name");
-		System.out.println(s2.getname());
-		System.out.println(s1.getprice());
-		//System.out.println(s1.getprice());
-		//assertEquals(1,s1.getorderQuantity());
-		//assertEquals("name",s1.getname());
-		//assertEquals(1.0,s1.getprice(),0);
-		//assertEquals(0.1,s1.gettaxRate(),0);
 		
-		//SalesOrder receipt=new SalesOrder();
-		//assertEquals(1,receipt.getQuantity());
+		//testing SalesOrderItem.getname/get quantity
+		assertEquals("bread",s1.getname(),"bread");
+		assertEquals(1,s1.getorderQuantity());
+		assertEquals("cheese",s2.getname(),"cheese");
+		assertEquals(1,s2.getorderQuantity());
+		
+		
+		s1.getprice();
+		s1.gettaxRate();
+		
+		//testing SalesOrderItem.getprice and taxrate from Inventory
+		
+		assertEquals(2.5,s2.getprice(),0.0);
+		assertEquals(0.0,s2.gettaxRate(),0.0);
+		// creat a new SalesOrder 
+		
+		SalesOrder receipt=new SalesOrder();
+		receipt.addItemList(s1);
+		receipt.addItemList(s2);
+		receipt.removeItemList("bread");
+		//s1has been removed from receipt
+		receipt.showReceipt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		//assertEquals("name",receipt.getname(),"name");
 		//assertEquals(1.0,receipt.getprice(),0);
 		//assertEquals(0.1,receipt.gettaxRate(),0);
 		//List<SalesOrderItem> itemlist=new ArrayList<SalesOrderItem>();
-				//receipt.addItemList(s1);
+		//System.out.println(s1.getprice());		
+				
 				//receipt.addItemList(s2);
 				//receipt.removeItemList(s1);
 		//String a=receipt.get(0).getItemID();
 	    //String b=receipt.get(1).getname();
-	    //for(int i=0;i<receipt.size();i++)
+	    //for(int i=0;i<(receipt.size());i++)
 	    //{
 	    //System.out.println("ITEM ID:"+" "+receipt.get(i).getItemID()+"\t"+"QUANTITY:"+" "+itemlist.get(i).getorderQuantity()+"\t"+
 	    //	"ITEM NAME:"+" "+itemlist.get(i).getname()+"\t"+"ITEM PRICE:"+" "+itemlist.get(i).getprice()+"\t"+"ITEM TAX:"+" "+itemlist.get(i).gettaxRate());
-	   // }
-	   // System.out.println(receipt.getTotalprice());
+	   //}
+	  // System.out.println(receipt.getTotalprice());
 	    
-	    //receipt.showReceipt();
+	    
 	}
 }
 
