813f086c8dd42500766feaccc1f44740f4b77e3a
wenjingggg <lu003374@stthomas.edu>
2016-11-04 15:24:41 -0500

    Update Register.java
    Added functions for :
    calculating each user daily(shift) sales total
    calculating each register machine total sales
    calculating each user total sales

--- POS_System/src/Register.java
+++ POS_System/src/Register.java
@@ -35,6 +35,9 @@ public class Register {
 	private SalesOrder receipt;
 	private double sessionTotalSale;
 	private int totalTransactions;
+	private double UserTotalfromLog=0.0;
+	private double UserShiftTotalfromLog=0.0;
+	private double RegisterTotalfromLog=0.0;
 	
 	public Register(int registerID){
 		this.registerID = registerID;
@@ -246,44 +249,70 @@ public class Register {
 			System.out.println("No pending receipt to display.");
 	}
 		
-	public void getRegisterTotalSale() throws NumberFormatException, IOException, ParseException{
+	public void getSaleAmount() throws NumberFormatException, IOException, ParseException{
 		
-			String path="./././res/RegisterSession.txt";
-			
-			BufferedReader bufferLog = null;
-			try {
-			    bufferLog= new BufferedReader(new FileReader(path));
-			    String singleLineLog = null;
-			    double RegisterTotalfromLog=0.0;
-			    singleLineLog=bufferLog.readLine();
-			    
-			    while ((singleLineLog = bufferLog.readLine()) != null) 
-			      {
-			    	String[] splited = singleLineLog.split("[|]");
-			    	if(splited[2].equals(getUserName()))
-			    	{
-			    		double sessionTotalformLog=Double.parseDouble(splited[6]);
-			    		RegisterTotalfromLog=RegisterTotalfromLog+sessionTotalformLog;
-			    	}
-			       
-			        
-			     }
-			    System.out.println(RegisterTotalfromLog);
-			} catch (IOException e) {
-			    System.out.println("There was a problem: " + e);
-			    e.printStackTrace();
-			} finally {
-			    try {
-			        bufferLog.close();
-			    } catch (Exception e) {
-			    }
-			}
+		String path="./././res/RegisterSession.txt";
 		
-
-		
+		BufferedReader bufferLog = null;
+		try {
+		    bufferLog= new BufferedReader(new FileReader(path));
+		    String singleLineLog = null;
+		    
+		    singleLineLog=bufferLog.readLine();
+		    
+		    while ((singleLineLog = bufferLog.readLine()) != null) 
+		      {
+		    	String[] splited = singleLineLog.split("[|]");
+		    	String WeekLogin=splited[4].substring(0,3);
+		    	String MonthLogin=splited[4].substring(4, 7);
+		    	String DayLogin=splited[4].substring(8, 10);
+		    	String WeekLogoff=splited[5].substring(0,3);
+		    	String MonthLogoff=splited[5].substring(4, 7); 
+		    	String DayLogoff=splited[5].substring(8, 10);
+		    	System.out.println(splited[1]);
+		    	double sessionTotalfromLog=Double.parseDouble(splited[6]);
+		    	if(splited[2].equals(getUserName()))
+		    		{
+		    		
+		    		if(WeekLogin.equals(WeekLogoff)&&MonthLogin.equals(MonthLogoff)&&DayLogin.equals(DayLogoff))
+		    			{
+		    			
+		    			UserShiftTotalfromLog=UserShiftTotalfromLog+sessionTotalfromLog;
+		    			UserTotalfromLog=UserTotalfromLog+sessionTotalfromLog;
+		    			}
+		    		}
+		    	if(splited[1].equals(String.valueOf((getRegisterID()))))
+		    	{
+		    		RegisterTotalfromLog=RegisterTotalfromLog+sessionTotalfromLog;
+		    	}
+		    	UserTotalfromLog=UserTotalfromLog+sessionTotalfromLog;
+		     }
+		    System.out.println(UserShiftTotalfromLog);
+		    System.out.println(UserTotalfromLog);
+		    System.out.println(RegisterTotalfromLog);
+		    
+		   
+		} catch (IOException e) {
+		    System.out.println("There was a problem: " + e);
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        bufferLog.close();
+		    } catch (Exception e) {
+		    }
+		}
+	
 	}
 		
+	public double getUserShiftTotal(){
+		return UserShiftTotalfromLog;
+	}
+	public double getUserTotalSale(){
+		return UserTotalfromLog;
 		
-	
+	}
+	public double getRegisterTotal(){
+		return RegisterTotalfromLog;
+	}
 	
 }
